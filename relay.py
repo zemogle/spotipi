18846fbfe7fe46c1c55718d0b5cbd0aeb891f333
Edward Gomez <edward@gomez.me.uk>
2020-11-29 13:40:57 +0000

    Look up device id if failure

--- relay.py
+++ relay.py
@@ -24,16 +24,25 @@ def end_read(signal,frame):
     rdr.cleanup()
     sys.exit()
 
+def get_device_id(devicelist):
+    devices = {d['name']:d['id'] for d in devicelist['devices']}
+    # Find ID of required device
+    return devices.get(DEVICE, None)
 
 def spotify_init():
-    return spotipy.Spotify(auth_manager=SpotifyOAuth(username=USERNAME,
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(username=USERNAME,
             scope=SCOPES,
             client_id=CLIENTID,
             client_secret=CLIENTSECRET,
             show_dialog=False,
             redirect_uri='http://www.gomez.me.uk/'))
+    device_id = get_device_id(sp.devices())
+    return sp, device_id
 
 def spotify_randomiser(token):
+    '''
+    Currently not implemented
+    '''
     sp = spotipy.Spotify(auth=token)
     pl = sp.user_playlist(user=USERNAME,playlist_id='spotify:playlist:{}'.format(PLAYLISTID))
     tracknum = random.randint(0,pl['tracks']['total'])-1
@@ -43,21 +52,20 @@ def spotify_randomiser(token):
     sp.start_playback(device_id=DEVICE, uris=[track])
     return True
 
-def spotify_play_track(sp, trackuri):
-    # if not sp.currently_playing() or  not sp.currently_playing().get('is_playing', False):
-    sp.start_playback(device_id=DEVICE, uris=[trackuri])
-    sp.volume(volume_percent=40)
+def spotify_play_track(sp, trackuri, device_id):
+    if device_id
+        sp.start_playback(device_id=device_id, uris=[trackuri])
+    else:
+        sp.start_playback(uris=[trackuri])
+    sp.volume(volume_percent=30)
     return True
-    # else:
-    #     print('Something is already playing')
-    #     return False
 
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, end_read)
 
     print("Starting")
-    sp = spotify_init()
+    sp, device_id = spotify_init()
     while run:
         rdr.wait_for_tag()
 
@@ -73,9 +81,9 @@ if __name__ == '__main__':
                 print('Track not found for {tagid}')
                 continue
             try:
-                sent = spotify_play_track(sp, trackuri)
+                sent = spotify_play_track(sp, trackuri, device_id)
                 time.sleep(5) #Delay before checking the tag reader again
             except SpotifyException as e:
-                print('Trying again to get token {}'.format(e))
-                sp = spotify_init()
-                sent = spotify_play_track(sp, trackuri)
+                print('Trying again to get token and device {}'.format(e))
+                sp, device_id = spotify_init()
+                sent = spotify_play_track(sp, trackuri, device_id)
